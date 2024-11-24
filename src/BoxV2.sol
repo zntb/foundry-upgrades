2fd3430528b78fe7808b8dd8dfb799e67768c657
zntb <zenetibi@yahoo.com>
2024-11-24 11:14:17 +0200

    OpenZeppelin UUPS Proxies

---
+++ src/BoxV2.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV2 is UUPSUpgradeable {
+    uint256 internal number;
+
+    function setNumber(uint256 _number) external {}
+
+    function getNumber() public view returns (uint256) {
+        return number;
+    }
+
+    function version() public pure returns (uint256) {
+        return 2;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
