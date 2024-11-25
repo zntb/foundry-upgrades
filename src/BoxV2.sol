725e41ee694aa0c4ae03b325b3d8c187ac97a924
zntb <zenetibi@yahoo.com>
2024-11-25 10:07:00 +0200

    Testing UUPS proxies

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -6,7 +6,9 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/U
 contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
 
-    function setNumber(uint256 _number) external {}
+    function setNumber(uint256 _number) external {
+        number = _number;
+    }
 
     function getNumber() public view returns (uint256) {
         return number;
---
+++ test/DeployAndUpgradeTest.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {DeployBox} from "../script/DeployBox.s.sol";
+import {UpgradeBox} from "../script/UpgradeBox.s.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+import {BoxV2} from "../src/BoxV2.sol";
+
+contract DeployAndUpgradeTest is Test {
+    DeployBox public deployer;
+    UpgradeBox public upgrader;
+    address public OWNER = makeAddr("owner");
+
+    address public proxy;
+
+    function setUp() public {
+        deployer = new DeployBox();
+        upgrader = new UpgradeBox();
+        proxy = deployer.run();
+    }
+
+    function testProxyStartAsBoxV1() public {
+        vm.expectRevert();
+        BoxV2(proxy).setNumber(7);
+    }
+
+    function testUpgrades() public {
+        BoxV2 box2 = new BoxV2();
+
+        upgrader.upgradeBox(proxy, address(box2));
+
+        uint256 expectedValue = 2;
+        assertEq(expectedValue, BoxV2(proxy).version());
+
+        BoxV2(proxy).setNumber(7);
+        assertEq(7, BoxV2(proxy).getNumber());
+    }
+}
